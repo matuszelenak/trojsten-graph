c0863b06da8dea7c55c8da28b69b095cfb63d597
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2019-10-26 17:47:38 +0200

    Initial fixtures and some admins

--- graph/people/admin.py
+++ graph/people/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.admin import TabularInline
 
-# Register your models here.
+from people.models import Person, Group, Relationship, RelationshipStatus
+
+
+class NoteInline(TabularInline):
+    model = Person.notes.through
+    extra = 0
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'nickname', 'birth_date')
+    inlines = (NoteInline,)
+    exclude = ('notes',)
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'parent')
+
+
+class RelationshipStatusInline(admin.TabularInline):
+    model = RelationshipStatus
+    extra = 0
+    fields = ('date_start', 'date_end', 'status')
+    ordering = ['date_start']
+
+
+@admin.register(Relationship)
+class RelationshipAdmin(admin.ModelAdmin):
+    readonly_fields = ('first_person', 'second_person')
+    inlines = (RelationshipStatusInline, )
+
+
+@admin.register(RelationshipStatus)
+class RelationshipStatusAdmin(admin.ModelAdmin):
+    pass
