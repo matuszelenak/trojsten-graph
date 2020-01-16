e320d9ddcd4083807ae0027609077f8a8249a52a
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2020-01-16 15:01:20 +0100

    Repo cleanup (remove heroku)

--- graph/graph/settings/heroku.py
+++
@@ -1,5 +0,0 @@
-from .production import *
-import dj_database_url
-
-
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
--- graph/heroku.deployment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python manage.py migrate
-python manage.py loaddata initial
-python manage.py collectstatic --noinput
-python manage.py build_enums
-
-gunicorn --bind 0.0.0.0:$PORT graph.wsgi --env DJANGO_SETTINGS_MODULE=graph.settings.heroku --log-level DEBUG --access-logfile - --log-file -
