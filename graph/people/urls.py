5da0360f7df34ddfc17f4e8e16a238ecb0fc9fda
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2020-01-16 20:38:27 +0100

    Add about & help texts, refactor suggestion model

--- graph/people/urls.py
+++ graph/people/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     url(r'^$', login_required(views.GraphView.as_view()), name='graph'),
     url(r'^graph-data/$', login_required(views.GraphDataView.as_view()), name='graph_data'),
+    url(r'^about/$', login_required(views.AboutView.as_view()), name='about')
 ]
