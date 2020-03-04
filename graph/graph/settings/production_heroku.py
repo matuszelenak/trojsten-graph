5294e4850f8571aabb906fcfb8778f7067546b2a
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2020-03-04 01:21:16 +0100

    Get fucked whitenoise

--- graph/graph/settings/production_heroku.py
+++ graph/graph/settings/production_heroku.py
@@ -4,18 +4,18 @@ from .production import *
 
 PARENT_HOST = 'graph.trojsten.wtf'
 
-MIDDLEWARE = list(MIDDLEWARE)
-MIDDLEWARE.remove('django_hosts.middleware.HostsRequestMiddleware')
-MIDDLEWARE = tuple(MIDDLEWARE)
-
-MIDDLEWARE = (
-    'django_hosts.middleware.HostsRequestMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-) + MIDDLEWARE
-
-INSTALLED_APPS += ['whitenoise']
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# MIDDLEWARE = list(MIDDLEWARE)
+# MIDDLEWARE.remove('django_hosts.middleware.HostsRequestMiddleware')
+# MIDDLEWARE = tuple(MIDDLEWARE)
+#
+# MIDDLEWARE = (
+#     'django_hosts.middleware.HostsRequestMiddleware',
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+# ) + MIDDLEWARE
+#
+# INSTALLED_APPS += ['whitenoise']
+#
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ADMINS = [('Matus Zelenak', 'matus.zelenak@trojsten.com')]
 
