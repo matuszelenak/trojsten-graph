9b2f408838dd49e9ec7f0c634f18b68e990fd12f
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2022-01-14 13:25:01 +0100

    fix urls

--- graph/graph/settings/base.py
+++ graph/graph/settings/base.py
@@ -154,7 +154,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'graph'
+LOGIN_REDIRECT_URL = 'gdpr'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
