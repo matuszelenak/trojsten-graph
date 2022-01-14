feba3e061263967c7600095f14e584e23b1a564b
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2022-01-14 13:24:37 +0100

    fix urls

--- graph/people/urls.py
+++ graph/people/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.GDPRView.as_view(), name='graph'),
+    url(r'^$', views.GDPRView.as_view(), name='gdpr'),
     url(r'^bravosixgoingdark/$', login_required(views.GraphView.as_view()), name='graph'),
     url(r'^graph-data/$', login_required(views.GraphDataView.as_view()), name='graph_data'),
     # url(r'^about/$', login_required(views.AboutView.as_view()), name='about')
