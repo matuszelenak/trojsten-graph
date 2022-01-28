97679034023b7f729d4130acdee15e041ef5d8b0
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2022-01-28 18:33:11 +0100

    Split content management to multiple pages

--- graph/people/views.py
+++ graph/people/views.py
@@ -1,16 +1,14 @@
-from typing import Tuple, List
-
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.db import transaction
 from django.db.models import Q
-from django.forms import ModelForm, BaseModelFormSet
+from django.forms import ModelForm
 from django.http import JsonResponse, HttpResponseNotFound, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 
-from people.forms import RelationshipStatusFormset, PersonForm, GroupMembershipFormset
+from people.forms import RelationshipStatusFormset, PersonForm, GroupMembershipFormset, DeletionForm
 from people.models import Person, Relationship
 from people.serializers import PeopleSerializer, RelationshipSerializer
 
@@ -41,10 +39,26 @@ class AboutView(TemplateView):
     template_name = 'people/about.html'
 
 
-class ContentManagementView(TemplateView):
-    template_name = 'people/content_management.html'
+class PersonContentManagementView(FormView):
+    template_name = 'people/content_management/person.html'
+    form_class = PersonForm
+    success_url = reverse_lazy('person-content-management')
 
-    def get_forms(self, request) -> Tuple[ModelForm, BaseModelFormSet, List[BaseModelFormSet]]:
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.request.user
+        return kwargs
+
+    @transaction.atomic
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class RelationshipsContentManagementView(TemplateView):
+    template_name = 'people/content_management/relationships.html'
+
+    def get_form(self, request):
         person = request.user
         relationships = Relationship.objects.filter(
             Q(first_person=person) | Q(second_person=person)
@@ -55,47 +69,25 @@ class ContentManagementView(TemplateView):
         else:
             extra_kwargs = {}
 
-        return (
-                PersonForm(instance=person, prefix='person', **extra_kwargs),
-                GroupMembershipFormset(instance=person, prefix='groups', **extra_kwargs),
-                [
-                    RelationshipStatusFormset(
-                        instance=r,
-                        prefix=f"relationship_{r.id}",
-                        queryset=r.statuses.with_confirmation_status(person).order_by('date_start', 'date_end'),
-                        **extra_kwargs
-                    )
-                    for r in relationships
-                ]
+        return [
+            RelationshipStatusFormset(
+                instance=r,
+                prefix=f"relationship_{r.id}",
+                queryset=r.statuses.with_confirmation_status(person).order_by('date_start', 'date_end'),
+                **extra_kwargs
             )
-
+            for r in relationships
+        ]
 
     def get(self, request, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        person_form, groups, relationships = self.get_forms(request)
-        context['person_form'] = person_form
-        context['group_memberships'] = groups
-        context['relationships'] = relationships
+        context['relationships'] = self.get_form(request)
         return self.render_to_response(context)
 
     @transaction.atomic
     def post(self, request, *args, **kwargs):
-        if 'delete_all' in request.POST:
-            user = request.user
-            logout(request)
-            user.delete()
-
-            return HttpResponseRedirect(reverse('login'))
-
-        person_form, groups, relationships = self.get_forms(request)
-
-        if person_form.is_valid() and groups.is_valid() and all([x.is_valid() for x in relationships]):
-            if person_form.has_changed():
-                person_form.save()
-
-            if groups.has_changed():
-                groups.save()
-
+        relationships = self.get_form(request)
+        if all([x.is_valid() for x in relationships]):
             relationship_form: ModelForm
             for relationship_form in relationships:
                 for status_form in relationship_form:
@@ -109,12 +101,41 @@ class ContentManagementView(TemplateView):
                     relationship_form.instance.delete()
 
             messages.success(request, "Changes were successfully saved")
-            return HttpResponseRedirect(reverse('content_management'))
+            return HttpResponseRedirect(reverse('relationships-content-management'))
         else:
             messages.error(request, "There were mistakes in the form")
 
         context = self.get_context_data(**kwargs)
-        context['person_form'] = person_form
-        context['group_memberships'] = groups
         context['relationships'] = relationships
         return self.render_to_response(context)
+
+
+class GroupContentManagementView(FormView):
+    form_class = GroupMembershipFormset
+    success_url = reverse_lazy('groups-content-management')
+    template_name = 'people/content_management/groups.html'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.request.user
+        return kwargs
+
+    @transaction.atomic
+    def form_valid(self, form):
+        if form.has_changed():
+            form.save()
+            messages.success(self.request, "Changes were successfully saved")
+
+        return super().form_valid(form)
+
+
+class DeletionManagementView(FormView):
+    form_class = DeletionForm
+    success_url = reverse_lazy('login')
+    template_name = 'people/content_management/delete.html'
+
+    def form_valid(self, form):
+        user = self.request.user
+        logout(self.request)
+        user.delete()
+        return super().form_valid(form)
