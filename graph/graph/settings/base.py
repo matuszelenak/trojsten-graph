3e894db765c661c4ca41b027b0f9349bb75d56ac
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2022-02-02 19:07:57 +0100

    Add email association to auth pipeline

--- graph/graph/settings/base.py
+++ graph/graph/settings/base.py
@@ -186,3 +186,16 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY")
 RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY")
 SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
