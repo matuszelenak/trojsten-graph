8a9fa5359a5d947be34aca8e3b9378f04680096a
Matus Zelenak <matus.zelenak.gjh@gmail.com>
2022-04-17 23:59:02 +0200

    Fix login/logout url reverse

--- graph/api/urls.py
+++ graph/api/urls.py
@@ -5,6 +5,6 @@ from api.views.login import login, logout, get_logged_user
 urlpatterns = [
     re_path(r'^get-authenticated-user/$', get_logged_user, name='get_authenticated_user'),
 
-    re_path(r'^login/$', login, name='login'),
-    re_path(r'^logout/$', logout, name='logout'),
+    re_path(r'^login/$', login, name='api-login'),
+    re_path(r'^logout/$', logout, name='api-logout'),
 ]
