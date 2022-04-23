7d312097054421f9cfc0f65197d0e18bc8fb361e
Matus Zelenak <matus.zelenak.gjh@gmail.com>
2022-04-23 14:41:39 +0200

    Add interface for contact submission

--- graph/people/urls.py
+++ graph/people/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import RedirectView
 from .views.content_management import PersonContentManagementView, RelationshipContentManagementView, \
     GroupContentManagementView, DeletionManagementView
 from .views.graph import GraphView, GraphDataView
+from .views.contact_gathering import EmailGatheringView
 
 urlpatterns = [
     re_path(r'^$', login_required(GraphView.as_view()), name='graph'),
@@ -14,6 +15,7 @@ urlpatterns = [
     re_path(r'^content-management/person/$', login_required(PersonContentManagementView.as_view()), name='person-content-management'),
     re_path(r'^content-management/relationship/(?P<pk>\d+)/$', login_required(RelationshipContentManagementView.as_view()), name='relationship-content-management'),
     re_path(r'^content-management/groups/$', login_required(GroupContentManagementView.as_view()), name='groups-content-management'),
-    re_path(r'^content-management/delete/$', login_required(DeletionManagementView.as_view()), name='deletion-content-management')
+    re_path(r'^content-management/delete/$', login_required(DeletionManagementView.as_view()), name='deletion-content-management'),
     # url(r'^about/$', login_required(views.AboutView.as_view()), name='about')
+    re_path(r'^gather-emails/$', (EmailGatheringView.as_view()), name='email_gather')
 ]
